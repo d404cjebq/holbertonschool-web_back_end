2bdb8c022db62f3a53f619e86c13ce99267d341e
d404cjebq <imdhaydhw@gmail.com>
2026-09-06 18:04:26 +0300

    add

--- NoSQL/ 11-schools_by_topic.py
+++ NoSQL/ 11-schools_by_topic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Module that finds schools having a specific topic."""
+"""Module that returns schools having a specific topic"""
 
 
 def schools_by_topic(mongo_collection, topic):
-    """Return the list of schools that include the given topic."""
+    """Returns the list of schools having a specific topic"""
     return list(mongo_collection.find({"topics": topic}))
