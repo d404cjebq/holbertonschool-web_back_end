5d398f250c7045168f03a71135d33a35a8adcfc0
d404cjebq <imdhaydhw@gmail.com>
2026-09-06 18:22:30 +0300

    11. Where can I learn Python?

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""Module that inserts a new document in a collection."""
+"""Module that inserts a new document in a collection"""
 
 
 def insert_school(mongo_collection, **kwargs):
-    """Insert a document based on kwargs and return the new _id."""
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    """Inserts a new document in a collection based on kwargs"""
+    return mongo_collection.insert_one(kwargs).inserted_id
